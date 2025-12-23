d0a63920cfc78c66ba8c590b3af0146159a73e23
Zaid Alwash <zaidmustafaalwash@gmail.com>
2025-12-23 18:56:12 -0500

    Final submission

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,16 +1,21 @@
 import { MongoClient } from 'mongodb';
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+// Read the Mongo URI from environment variables
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error(
+    "Please add your Mongo URI as an environment variable named 'MONGODB_URI'"
+  );
 }
 
-const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
+  // Use a global variable to preserve the client across hot reloads
   let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
@@ -21,9 +26,9 @@ if (process.env.NODE_ENV === 'development') {
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
+  // In production, create a new client
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
 export default clientPromise;
-
